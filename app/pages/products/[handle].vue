b6e39b2f69c1b5c4d60c495592c108414b22b367
alvinlaroya <alvinreggaelaroya@gmail.com>
2025-07-19 05:21:05 +0800

    fix

--- app/pages/products/[handle].vue
+++ app/pages/products/[handle].vue
@@ -31,13 +31,15 @@ useSeoMeta({
 <template>
     <NuxtLayout name="products" class="">
         <div class="flex flex-col gap-12">
+            <h1>TEST</h1>
             <div class="grid grid-cols-12 w-full py-4 gap-10">
-                <div class="col-span-7">
-                    <NuxtImg :src="images[0]?.node?.previewImage?.url"
-                        class="rounded-lg"
-                        :alt="images[0]?.node?.previewImage?.altText || 'main-img'" fit="cover" format="webp"
-                        width="700" height="700" :modifiers="{ crop: 'center', padColor: 'ecebeb' }" preload />
+                <div class="col-span-12 lg:col-span-7">
+                    <div class="overflow-hidden">
+                        <NuxtImg :src="images[0]?.node?.previewImage?.url" class="rounded-lg"
+                            :alt="images[0]?.node?.previewImage?.altText || 'main-img'" fit="cover" format="webp"
+                            width="700" height="700" :modifiers="{ crop: 'center', padColor: 'ecebeb' }" preload />
 
+                    </div>
                     <div class="flex justify-between gap-4 mt-4 ">
                         <div v-for="(img, i) in images.slice(0, 5)" :key="i">
                             <NuxtImg :src="img?.node?.previewImage?.url" class="border bg-[#ecebeb] rounded-md"
@@ -46,7 +48,7 @@ useSeoMeta({
                         </div>
                     </div>
                 </div>
-                <div class="col-span-5">
+                <div class="col-span-12 lg:col-span-5">
                     <div class="flex flex-col gap-5">
                         <h2 class="text-4xl">{{ product.title }}</h2>
                         <span class="text-xl font-semibold">${{ product.priceRange.maxVariantPrice.amount }}</span>
@@ -66,6 +68,9 @@ useSeoMeta({
                             :compare-at-price-range="product.compareAtPriceRange" :price-range="product.priceRange" />
                     </NuxtLink>
                 </div>
+            </div>
+            <div>
+                <pre>{{ product }}</pre>
             </div>
         </div>
     </NuxtLayout>
